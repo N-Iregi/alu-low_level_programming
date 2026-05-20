1958ef69bd357a4f4c0aa1de21ce3d7f8913d156
N-Iregi <ireginev5@gmail.com>
2026-05-20 18:59:24 +0300

    Add task 8

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	/* write() writes directly to a file descriptor */
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 60);
+	return (1);
+}
